1c1e01bdb1fa0cefc1ae852be6b3f3c17b423632
GIGOMEDIA <gratitudeowoade200@gmail.com>
2025-10-27 21:45:37 +0100

    display issue fixed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-ticket/'  // <-- ensures assets load correctly on GitHub Pages
+  server: {
+    port: 4000,
+  }  // <-- ensures assets load correctly on GitHub Pages
 })
 
