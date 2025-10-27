ac147dfaeb13d088229aa0cb4772fad51331e3e0
GIGOMEDIA <gratitudeowoade200@gmail.com>
2025-10-27 17:55:48 +0100

    Switch to hash routing and prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-ticket/'  // <-- add this line for GitHub Pages
+  base: '/vue-ticket/'   // important for GitHub Pages
 })
+
